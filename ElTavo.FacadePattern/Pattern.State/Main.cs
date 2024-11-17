312e5991d528cffe9793070c38ef5d3824328012
Raúl Armas <raularmasbx@gmail.com>
2024-11-17 00:44:53 -0500

    update

---
+++ ElTavo.FacadePattern/Pattern.State/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.State
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.SelectCoffee();
+            coffeeMachine.InsertCoin();       // Moneda insertada
+            coffeeMachine.SelectCoffee();     // Café seleccionado
+            coffeeMachine.DispenseCoffee();   // Dispensando café
+
+            coffeeMachine.DispenseCoffee();   // Error: no hay moneda
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
