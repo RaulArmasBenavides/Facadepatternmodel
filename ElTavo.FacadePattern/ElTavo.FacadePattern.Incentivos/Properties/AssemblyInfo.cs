f7eb62eb224c58df1626d45ecc24ded1bd84c05e
Raul Armas <raul.armas@intelica.com>
2024-11-20 12:13:53 -0500

    update

--- ElTavo.FacadePattern/ElTavo.FacadePattern.Incentivos/Properties/AssemblyInfo.cs
+++ ElTavo.FacadePattern/ElTavo.FacadePattern.Incentivos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ElTavo.FacadePattern.Incentivos")]
+[assembly: AssemblyTitle("FacadePattern.Incentivos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElTavo.FacadePattern.Incentivos")]
+[assembly: AssemblyProduct("FacadePattern.Incentivos")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
