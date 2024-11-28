4e2fbd69f836b93ee0006b6e6e58df8b6fc9e72f
Raul Armas <raul.armas@intelica.com>
2024-11-28 18:31:52 -0500

    memento pattern

---
+++ FacadePattern/Pattern.Memento/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var editor = new TextEditor();
+            var history = new History();
+
+            editor.SetContent("Versión 1");
+            history.Push(editor.Save()); // Guardar estado
+            Console.WriteLine("Contenido actual: " + editor.GetContent());
+
+            editor.SetContent("Versión 2");
+            history.Push(editor.Save()); // Guardar estado
+            Console.WriteLine("Contenido actual: " + editor.GetContent());
+
+            editor.SetContent("Versión 3");
+            Console.WriteLine("Contenido actual: " + editor.GetContent());
+
+            // Deshacer cambios
+            if (history.HasHistory())
+            {
+                editor.Restore(history.Pop());
+                Console.WriteLine("Deshacer: " + editor.GetContent());
+            }
+
+            if (history.HasHistory())
+            {
+                editor.Restore(history.Pop());
+                Console.WriteLine("Deshacer: " + editor.GetContent());
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FacadePattern/Pattern.Memento/TextEditor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.Memento
+{
+    public class TextEditor
+    {
+        private string _content;
+
+        public void SetContent(string content)
+        {
+            _content = content;
+        }
+
+        public string GetContent()
+        {
+            return _content;
+        }
+
+        public Memento Save()
+        {
+            return new Memento(_content);
+        }
+
+        public void Restore(Memento memento)
+        {
+            _content = memento.State;
+        }
+    }
+
+    public class History
+    {
+        private readonly Stack<Memento> _mementos = new();
+
+        public void Push(Memento memento)
+        {
+            _mementos.Push(memento);
+        }
+
+        public Memento Pop()
+        {
+            return _mementos.Pop();
+        }
+
+        public bool HasHistory()
+        {
+            return _mementos.Count > 0;
+        }
+    }
+}
