28a65942dd7d5cc97c22dc4e622117cc087353a9
Raul Armas <raul.armas@intelica.com>
2024-12-17 12:06:11 -0500

    update

---
+++ FacadePattern/Pattern.Flyweitght/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.Flyweitght
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new VehicleTypeFactory();
+
+            // Crear tipos de vehículos (Flyweights)
+            var carType = factory.GetVehicleType("Car", "car-icon.png");
+            var truckType = factory.GetVehicleType("Truck", "truck-icon.png");
+            var bikeType = factory.GetVehicleType("Motorcycle", "bike-icon.png");
+
+            // Crear vehículos
+            var vehicles = new List<Vehicle>
+        {
+            new Vehicle(10, 20, carType),
+            new Vehicle(15, 25, truckType),
+            new Vehicle(20, 30, carType),
+            new Vehicle(25, 35, bikeType),
+            new Vehicle(30, 40, truckType)
+        };
+
+            // Renderizar vehículos
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Render();
+            }
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FacadePattern/Pattern.Flyweitght/VehicleType.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern.Flyweitght
+{
+    public class VehicleType
+    {
+        public string Name { get; }
+        public string Icon { get; }  
+
+        public VehicleType(string name, string icon)
+        {
+            Name = name;
+            Icon = icon;
+        }
+
+        public void Render(int x, int y)
+        {
+            Console.WriteLine($"Rendering {Name} at ({x}, {y}) with icon {Icon}");
+        }
+    }
+
+    public class VehicleTypeFactory
+    {
+        private Dictionary<string, VehicleType> _vehicleTypes = new Dictionary<string, VehicleType>();
+
+        public VehicleType GetVehicleType(string name, string icon)
+        {
+            if (!_vehicleTypes.ContainsKey(name))
+            {
+                _vehicleTypes[name] = new VehicleType(name, icon);
+                Console.WriteLine($"Created new VehicleType: {name}");
+            }
+            return _vehicleTypes[name];
+        }
+    }
+
+    public class Vehicle
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public VehicleType Type { get; }
+
+        public Vehicle(int x, int y, VehicleType type)
+        {
+            X = x;
+            Y = y;
+            Type = type;
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Render()
+        {
+            Type.Render(X, Y);
+        }
+    }
+}
